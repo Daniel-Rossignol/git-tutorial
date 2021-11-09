768d75513d80ddbcf12c647cc13b7e3a6fe877c7
Alex Hoffman <alxhoff@gmail.com>
2021-11-09 22:45:23 +0100

    main reads back your number with help of library

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include "espl_lib.h"
+
 
 int main()
 {
-    printf("Hello ESPL");
+	printf("Please input a number: ");
 
-    return 0;
+	int input = 0;
+	char validation = 'n';	
+
+	do{
+		if(!scanf("%d", &input) || input <= 0){
+			printf("Not a positive number");
+			return -1;
+		}
+	printf("your number is %s" , num_to_words((unsigned int) input));
+	printf("\n New number (y) or exit (n)?");
+	scanf(" %c", &validation);	
+
+	} while(validation ==  'y');
+	
+	return 0;
 }
